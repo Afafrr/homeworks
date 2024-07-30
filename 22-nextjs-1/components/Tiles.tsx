73aa2865175534eadf9dcea32056f885a8b1590f
Afafrr <mateusz.aftanski@gmail.com>
2024-07-30 19:23:52 +0200

    add unique avatar check

--- 22-nextjs-1/components/Tiles.tsx
+++ 22-nextjs-1/components/Tiles.tsx
@@ -7,24 +7,37 @@ import { fetchAvatars } from "@/services/fetchAvatars";
 
 export const Tiles = ({ data }: { data: Avatar[] }) => {
   const [avatarList, setAvatarList] = useState<Avatar[]>([]);
-  const [clientData, setClientData] = useState<Avatar[]>([]);
+  const [clientData, setClientData] = useState<Avatar[]>();
   useEffect(() => {
     setClientData(data);
   }, []);
-  //fetch new data one image before
+  const fetchUniqueAvatars = async (num: number) => {
+    try {
+      const res = await fetchAvatars(num);
+      const uniqueArr = res.filter((item: Avatar) => {
+        const unique = avatarList.every((avatar) => avatar.id !== item.id);
+        if (unique) return item;
+      });
+      if (uniqueArr.length === 0) fetchUniqueAvatars(num);
+      return uniqueArr;
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  };
   const handleAvatarAddition = async () => {
-    const avatar = clientData ? clientData.shift() : null;
+    let avatar;
+    if (clientData) avatar = clientData.shift();
     if (avatar) {
       setAvatarList([...avatarList, avatar]);
       if (clientData && clientData.length < 2) {
-        const res = await fetchAvatars(5);
-        setClientData(res);
+        const uniqueArr = await fetchUniqueAvatars(5);
+        setClientData(uniqueArr);
       }
     }
   };
   //one avatar refresh
   const handleAvatarRefresh = async (id: number) => {
-    const res = await fetchAvatars(1);
+    const res = await fetchUniqueAvatars(1);
     avatarList.map((avatar, index) => {
       if (avatar.id === id) {
         const newArr = [...avatarList];
@@ -44,8 +57,8 @@ export const Tiles = ({ data }: { data: Avatar[] }) => {
     <div className={styles.tiles__container}>
       {avatarList.map((item) => (
         <AvatarTile
-          key={item.id}
-          imgSrc={item.url}
+          key={item?.id}
+          imgSrc={item?.url}
           onClick={() => handleAvatarRefresh(item.id)}
         />
       ))}
--- 22-nextjs-1/services/fetchAvatars.tsx
+++ 22-nextjs-1/services/fetchAvatars.tsx
@@ -1,13 +1,8 @@
-import { Avatar } from "@/types/types";
-export const fetchAvatars = async (num: number): Promise<Avatar[]> => {
+export const fetchAvatars = async (num: number) => {
   const url = `https://tinyfac.es/api/data?limit=${num}&quality=0`;
   return fetch(url)
     .then((res) => res.json())
     .then((res) => {
       return res;
-    })
-    .catch((err) => {
-      console.log(err);
-      throw new Error(err.message);
     });
 };
