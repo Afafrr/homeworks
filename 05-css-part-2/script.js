afb18710c29a373a7f536142c3db57f9e00cd058
Afafrr <mateusz.aftanski@gmail.com>
2024-08-04 11:39:49 +0200

    cleaned

--- 05-css-part-2/script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".theme__button");
-const scrollBtn = document.querySelector(".footer__scroll-top");
-
-button.addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-});
-scrollBtn.addEventListener("click", () => {
-  scrollTo({ top: 0, behavior: "smooth" });
-});
