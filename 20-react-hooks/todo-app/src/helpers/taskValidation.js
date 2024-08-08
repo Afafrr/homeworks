1931382f5457404606095cf872e8d1aab1c0b39c
Afafrr <mateusz.aftanski@gmail.com>
2024-08-08 18:56:18 +0200

    task finished

---
+++ 20-react-hooks/todo-app/src/helpers/taskValidation.js
@@ -0,0 +1,8 @@
+export const taskValidation = (text) => {
+  const INVALID_CHARACTERS = /[@#$%^&*)(+=_-]/g;
+
+  if (INVALID_CHARACTERS.test(text)) return "Invalid characters!";
+  if (text.length > 30) return "Task name too long! (max 30 characters)";
+  if (text.trim() === "") return "Task cannot be empty!";
+  return false;
+};
